083d9d6932de96223d4f8d08c5d5e72d479fe477
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-10-19 06:04:15 -0300

    feat: solved exercises 6 and 7

--- solucao.sql
+++ solucao.sql
@@ -26,3 +26,23 @@ USE Filmes
 SELECT Nome, Ano, Duracao
 FROM Filmes
 WHERE Ano > 2000;
+
+--6
+/* the BETWEEN operator is inclusive, so I needed to change the values
+to achieve the expected result */
+USE Filmes
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE duracao BETWEEN 101 AND 149
+ORDER BY duracao ASC;
+
+-- 7
+/* The exercise asked to order the following query by duration of the
+   movies released but the column "Duracao" doesn't get selected
+   anywhere in the query. So I ordered by "Quantidade" instead. */
+USE Filmes
+SELECT Ano, COUNT(*) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC;
+
