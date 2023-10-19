f07b75b3b51405747150a00261cb212dfd2f55d6
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-10-19 05:13:03 -0300

    feat: solved exercises 3 to 5

--- solucao.sql
+++ solucao.sql
@@ -8,3 +8,21 @@ USE Filmes
 SELECT Nome, Ano, Duracao
 FROM Filmes
 ORDER BY Ano ASC;
+
+-- 3
+USE Filmes
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro';
+
+-- 4
+USE Filmes
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Ano = 1997;
+
+-- 5
+USE Filmes
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Ano > 2000;
